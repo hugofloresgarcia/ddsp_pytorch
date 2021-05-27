5aec83e932056d517b3f635a5b836372c280e2f5
Hugo Flores Garcia <hf01049@georgiasouthern.edu>
2021-05-26 23:29:38 -0500

    refactor for gru encoder

---
+++ ddsp/models/encoder.py
@@ -0,0 +1,21 @@
+import ddsp
+
+import librosa
+import torch
+import torch.nn as nn
+
+class MFCCEncoder(nn.Module):
+
+    def __init__(self, sample_rate: int, block_size: int,
+                 hidden_size: int, n_mfccs: int, z_dim: int = None):
+        self.hidden_size = hidden_size
+        self.z_dim = z_dim
+
+        self.norm = nn.LayerNorm(n_mfccs)
+        self.gru = ddsp.gru(n_mfccs, hidden_size)
+        self.proj = nn.Linear(hidden_size, z_dim)
+
+
+    def forward(self, mfccs: torch.Tensor):
+
+        z = 
